a3fdea41af0093af05c6e3bd44d6639f98c3c90c
sagar535 <dsl.sagar535@gmail.com>
2022-07-18 13:12:49 +0545

    eslint fixes

--- pages/admin/users/[id].js
+++ pages/admin/users/[id].js
@@ -1,7 +1,6 @@
 import {useState, useEffect} from 'react'
 import {connect} from 'react-redux'
 import {useRouter} from 'next/router'
-import axios from 'axios'
 import Navbar from '../../../components/layout/Navbar'
 import {
   Btn,
