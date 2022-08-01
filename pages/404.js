27cae7d92bf5c41d76ec53650a9f26ed0038512c
sangam <snngm.sth@gmail.com>
2022-08-01 16:02:49 +0545

    add: readded custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import Navbar from '../components/layout/Navbar'
+
+const NotFound = () => {
+  const router = useRouter()
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.back()
+    }, 3000)
+  }, [])
+  return (
+    <>
+      <Navbar />
+      <div className="not-found">
+        <h2>This page cannot be found :(</h2>
+        <p>You will be redirected to previous page in 3 seconds...</p>
+        <p>
+          Go back to the{' '}
+          <Link href="/">
+            <a>Homepage</a>
+          </Link>
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default NotFound
