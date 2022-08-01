ae531b307d12b701c932f8ad289a50f32540eb84
sangam <snngm.sth@gmail.com>
2022-08-01 16:07:16 +0545

    add: Navbar for other error pages and route back to previous page

--- pages/_error.js
+++ pages/_error.js
@@ -1,7 +1,27 @@
-const Error = ({ statusCode }) =>
-  statusCode
-    ? `An error ${statusCode} occurred on server`
-    : 'An error occurred on client'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import Navbar from '../components/layout/Navbar'
+
+const Error = ({ statusCode }) => {
+  const router = useRouter()
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.back()
+    }, 3000)
+  }, [])
+
+  return (
+    <>
+      <Navbar />
+      <p>
+        {statusCode
+          ? `An error ${statusCode} occurred on server`
+          : 'An error occurred on client'}
+      </p>
+    </>
+  )
+}
 
 Error.getInitialProps = ({ res, err }) => {
   const cerr = err ? err.statusCode : 404
