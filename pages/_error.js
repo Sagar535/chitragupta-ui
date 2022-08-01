677e3b570ff9835bc8c235b8c0d1b44edcaab983
sangam <snngm.sth@gmail.com>
2022-08-01 12:04:55 +0545

    error page

--- pages/_error.js
+++ pages/_error.js
@@ -1,9 +1,41 @@
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import Link from 'next/link'
+import Navbar from '../components/layout/Navbar'
+
 function Error({ statusCode }) {
+  const router = useRouter()
+  useEffect(() => {
+    setTimeout(() => {
+      router.back()
+    }, 3000)
+  }, [])
+
   return (
     <p>
-      {statusCode
-        ? `An error ${statusCode} occurred on server`
-        : 'An error occurred on client'}
+      <Navbar />
+      {statusCode ? (
+        <>
+          {statusCode == 404 ? (
+            <>
+              <div className="not-found">
+                <h2>This page cannot be found :(</h2>
+                <p>You will be redirected to previous page in 3 seconds...</p>
+                <p>
+                  Go back to the{' '}
+                  <Link href="/">
+                    <a>Homepage</a>
+                  </Link>
+                </p>
+              </div>
+            </>
+          ) : (
+            `An error ${statusCode} occurred on server`
+          )}
+        </>
+      ) : (
+        'An error occurred on client'
+      )}
     </p>
   )
 }
